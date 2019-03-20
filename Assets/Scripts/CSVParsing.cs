099e3f4fc2d178efa7d71cf986333982fbde1155
Jacob Madden <jacob.madden@gmail.com>
2019-03-20 18:40:08 -0400

    sample project showing orbit

--- Assets/Scripts/CSVParsing.cs
+++ Assets/Scripts/CSVParsing.cs
@@ -1,35 +1,42 @@
 ﻿using UnityEngine;
 using System.Collections;
-using UnityEngine.UI;
 using System.IO;
- 
+
 public class CSVParsing : MonoBehaviour
 {
-public TextAsset csvFile; // Reference of CSV file
-public float scaleFactor;
-private char lineSeperator = '\n'; // It defines line seperate character
-private char fieldSeperator = ','; // It defines field seperate chracter
-private string[] records;
-private int curIndex = 1;
-public Text obsTime;
-void Start ()
-{
-readData ();
-}
-// Read data from CSV file
-private void readData(){
-    records = csvFile.text.Split(lineSeperator);
-}
+    public TextAsset csvFile;
 
-private void Update(){
-    string[] row = records[curIndex].Split(fieldSeperator);
-    //Debug.Log("Testing rows " + row[1]);
-    transform.position = new Vector3(float.Parse(row[1]) * scaleFactor, 
-    float.Parse(row[2]) * scaleFactor, float.Parse(row[3]) * scaleFactor);
-    //Time.timeScale = 10f;
-    obsTime.text = row[0];
-    curIndex = (curIndex + 5) % records.Length;
-    
-}
- 
+    private char lineSeperator = '\n';
+    private char fieldSeperator = ',';
+    private string[] records;
+
+    private void parseRecords()
+    {
+        records = csvFile.text.Split(lineSeperator);
+    }
+
+    private void Start()
+    {
+        parseRecords();
+    }
+
+    public string[] GetRecord(int index)
+    {
+        try
+        {
+            string[] row = records[index].Split(fieldSeperator);
+            return row;
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError(ex);
+            string[] row = new string[0];
+            return row;
+        }
+    }
+
+    public int GetNumRecords()
+    {
+        return records.Length;
+    }
 }
---
+++ Assets/Scripts/PositionController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PositionController : MonoBehaviour
+{
+    public Text obsTimeText;
+    public CSVParsing csvDataProvider;
+    public LineRenderer orbitLineRenderer;
+
+    public float positionScaleFactor = 0.001f;
+    public int recordSkipValue = 5;
+    public int orbitPositionInterval = 100;
+    //public float speed = 1.0f;
+
+    private int curIndex = 1;
+
+    void Start()
+    {
+    }
+
+    void AddPointToOrbit(Vector3 newPosition)
+    {
+        Vector3[] orbitPositionsArray = new Vector3[orbitLineRenderer.positionCount];
+        orbitLineRenderer.GetPositions(orbitPositionsArray);
+        Vector3[] updatedOrbitPositionsArray = new Vector3[orbitPositionsArray.Length + 1];
+        orbitPositionsArray.CopyTo(updatedOrbitPositionsArray, 0);
+        updatedOrbitPositionsArray[orbitPositionsArray.Length] = newPosition;
+        orbitLineRenderer.positionCount = updatedOrbitPositionsArray.Length;
+        orbitLineRenderer.SetPositions(updatedOrbitPositionsArray);
+    }
+
+    void Update()
+    {
+        try
+        {
+            string[] record = csvDataProvider.GetRecord(curIndex);
+            if (record.Length == 4)
+            {
+                obsTimeText.text = record[0];
+                Vector3 newPosition = new Vector3(float.Parse(record[1]),
+                    float.Parse(record[2]), float.Parse(record[3])) * positionScaleFactor;
+
+                // add a position to the orbit LineRenderer after an interval 
+                // to build up the orbit as the object goes through first orbit
+                if (curIndex > orbitLineRenderer.positionCount * orbitPositionInterval)
+                {
+                    AddPointToOrbit(newPosition);
+                }
+
+                transform.position = newPosition;
+
+                // this is an example of interpolating between time stamps towards a target position
+                // but really only makes sense when you have a few updates between target position
+                // updates
+                // comment out the transform update adove and uncomment speed when experimenting
+                //transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed);
+            }
+
+            curIndex = (curIndex + recordSkipValue) % csvDataProvider.GetNumRecords();
+            if (curIndex == 0)
+            {
+                curIndex = 1;
+            }
+        }
+        catch (System.Exception ex)
+        {
+            Debug.LogError("Error fetching record " + curIndex + " " + ex);
+        }
+    }
+}
